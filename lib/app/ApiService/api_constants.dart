734739509674efced5cddc38b210dd22c36e2b4d
Eray Tan <111294190+Eraytann@users.noreply.github.com>
2023-12-23 00:24:28 +0300

    Update api_constants.dart

--- lib/app/ApiService/api_constants.dart
+++ lib/app/ApiService/api_constants.dart
@@ -1,15 +1,1 @@
-class ApiChatGpt {
-  static const String baseUrl = "https://api.openai.com/v1/chat/completions";
-  static const String apiKey =
-      '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';
-}
 
-class ApiUberduck {
-  static const String baseUrl =
-      "https://api.uberduck.ai/reference-audio/backing-tracks?detailed=true";
-  static const String apiKey = 'pk_e7aba0e0-1f6e-4876-9734-7ab795743158';
-
-  static const String freeStyleUrl = 'https://api.uberduck.ai/tts/freestyle';
-  static const String freeStyleApiKey =
-      'cHViX2Rzamtqb25qdnd2a2JycXF1Zjpwa19jNWI1MmYwYi00MmFmLTQyZTItODFhNS1hOTQ0MDg3YWIzMGY=';
-}
